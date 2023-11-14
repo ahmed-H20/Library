bdde6076223a4a2aa1661d6319405afef2f0b9b6
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-11-14 17:21:53 +0200

    Make addBookToLibrary function
    it take args and make an object named book and store it in myLibrary array

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function Book(name, author, pages_num, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
-  
+function addBookToLibrary(name, author, pages_num, read) {
+  let book = new Book (name, author, pages_num, read);
+  myLibrary.push(book);
 }
