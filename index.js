34b55ac95cdc838e7abe50ac63e4a7fd7c84d1f9
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-11-14 17:16:19 +0200

    Make book function

---
+++ index.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(name, author, pages_num, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages_num;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+  
+}
