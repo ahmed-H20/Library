2ea6bde27d98877205d960394a7a4e8fbf929338
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-11-16 12:38:57 +0200

    Finish

--- index.js
+++ index.js
@@ -9,6 +9,39 @@ function Book(name, author, pages_num, read) {
     this.read = read;
 }
 
+Book.prototype.readable = function(){
+    this.read = !this.read;
+}
+
+function read(index){
+    myLibrary[index].readable();    
+    render();
+}
+
+function render() {
+    let library = document.querySelector(".library .container");
+    library.innerHTML = "";
+    for(let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i];
+        let bookcard = document.createElement("div");
+        bookcard.setAttribute("class", "card");
+        bookcard.innerHTML= `
+            <p>${book.name}</p>
+            <p>${book.author}</p>
+            <p>${book.pages}</p>
+            <button id="read" onclick="read(${i})" style="background-color: ${book.read ? "#22c55e": "#dc2626"}">${book.read ? "Readed" : "Not read yet"}</button>
+            <button id="remove" onclick="remove(${i})">Remove</button>        
+        `;        
+        library.appendChild(bookcard);
+    }
+    
+}
+
+function remove(index){
+    myLibrary.splice(index, 1);
+    render();
+}
+
 function addBookToLibrary() {
     let name = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
@@ -28,4 +61,5 @@ document.querySelector("#bookform").addEventListener("submit", function(){
     event.preventDefault();
     addBookToLibrary();
     bookForm.style.visibility="hidden";
+    render();
 })
